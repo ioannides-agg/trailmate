ee0617956b73b7673d9f4f30b6707fda0813c78b
elia <eliaetz@gmail.com>
2025-01-05 18:42:55 +0200

    Changed text color from black to white

--- Trailmate/Form1.cs
+++ Trailmate/Form1.cs
@@ -10,13 +10,14 @@ namespace Trailmate
             InitializeComponent();
             var skin = MaterialSkinManager.Instance;
             skin.AddFormToManage(this);
+
             skin.Theme = MaterialSkinManager.Themes.LIGHT;
             skin.ColorScheme = new ColorScheme(
                     Primary.Blue400,
                     Primary.Blue300,
                     Primary.Blue300,
                     Accent.Blue100,
-                    TextShade.BLACK
+                    TextShade.WHITE
                 );
         }
     }
