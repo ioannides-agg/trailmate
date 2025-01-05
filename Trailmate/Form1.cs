c5fa3ffd5b3bd317d0d38b53d93759eda4bf53ca
Aggelos Ioannides <ioannides.agg@gmail.com>
2025-01-05 18:26:00 +0200

    comitted project

---
+++ Trailmate/Form1.cs
@@ -0,0 +1,23 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace Trailmate
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var skin = MaterialSkinManager.Instance;
+            skin.AddFormToManage(this);
+            skin.Theme = MaterialSkinManager.Themes.LIGHT;
+            skin.ColorScheme = new ColorScheme(
+                    Primary.Blue400,
+                    Primary.Blue300,
+                    Primary.Blue300,
+                    Accent.Blue100,
+                    TextShade.BLACK
+                );
+        }
+    }
+}
