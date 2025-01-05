48398eaa1eb7be8f82e76e774132202818eb731d
Aggelos Ioannides <ioannides.agg@gmail.com>
2025-01-05 18:49:12 +0200

    Added tab control

--- Trailmate/Form1.Designer.cs
+++ Trailmate/Form1.Designer.cs
@@ -28,20 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.TabMaster = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TabMaster.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // TabMaster
+            // 
+            this.TabMaster.Controls.Add(this.tabPage1);
+            this.TabMaster.Controls.Add(this.tabPage2);
+            this.TabMaster.Depth = 0;
+            this.TabMaster.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabMaster.Location = new System.Drawing.Point(3, 64);
+            this.TabMaster.MouseState = MaterialSkin.MouseState.HOVER;
+            this.TabMaster.Multiline = true;
+            this.TabMaster.Name = "TabMaster";
+            this.TabMaster.SelectedIndex = 0;
+            this.TabMaster.Size = new System.Drawing.Size(794, 383);
+            this.TabMaster.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(786, 357);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TabMaster);
             this.Name = "Form1";
             this.Text = "TrailMate";
+            this.TabMaster.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialTabControl TabMaster;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
