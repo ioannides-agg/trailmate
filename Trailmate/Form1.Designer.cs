28a7f2c5d4dd3fd286945ebaa4b0bbf2fce74379
elia <eliaetz@gmail.com>
2025-01-07 22:46:55 +0200

    completed menu

--- Trailmate/Form1.Designer.cs
+++ Trailmate/Form1.Designer.cs
@@ -28,9 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.TabMaster = new MaterialSkin.Controls.MaterialTabControl();
-            this.setupPage = new System.Windows.Forms.TabPage();
             this.homePage = new System.Windows.Forms.TabPage();
+            this.setupPage = new System.Windows.Forms.TabPage();
             this.tentControlPage = new System.Windows.Forms.TabPage();
             this.lightControlPage = new System.Windows.Forms.TabPage();
             this.energyPage = new System.Windows.Forms.TabPage();
@@ -38,6 +40,7 @@
             this.explorePage = new System.Windows.Forms.TabPage();
             this.orderPage = new System.Windows.Forms.TabPage();
             this.eventsPage = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.TabMaster.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,107 +57,137 @@
             this.TabMaster.Controls.Add(this.eventsPage);
             this.TabMaster.Depth = 0;
             this.TabMaster.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TabMaster.Location = new System.Drawing.Point(4, 79);
-            this.TabMaster.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TabMaster.ImageList = this.imageList1;
+            this.TabMaster.Location = new System.Drawing.Point(3, 64);
             this.TabMaster.MouseState = MaterialSkin.MouseState.HOVER;
             this.TabMaster.Multiline = true;
             this.TabMaster.Name = "TabMaster";
             this.TabMaster.SelectedIndex = 0;
-            this.TabMaster.Size = new System.Drawing.Size(1059, 471);
+            this.TabMaster.Size = new System.Drawing.Size(894, 483);
             this.TabMaster.TabIndex = 0;
-            // 
-            // setupPage
-            // 
-            this.setupPage.Location = new System.Drawing.Point(4, 25);
-            this.setupPage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.setupPage.Name = "setupPage";
-            this.setupPage.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.setupPage.Size = new System.Drawing.Size(1051, 442);
-            this.setupPage.TabIndex = 0;
-            this.setupPage.Text = "Setup";
-            this.setupPage.UseVisualStyleBackColor = true;
             // 
             // homePage
             // 
-            this.homePage.Location = new System.Drawing.Point(4, 25);
+            this.homePage.ImageKey = "icons8-home-26.png";
+            this.homePage.Location = new System.Drawing.Point(4, 31);
+            this.homePage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.homePage.Name = "homePage";
-            this.homePage.Size = new System.Drawing.Size(1051, 442);
+            this.homePage.Size = new System.Drawing.Size(886, 448);
             this.homePage.TabIndex = 1;
             this.homePage.Text = "Home";
             this.homePage.UseVisualStyleBackColor = true;
             // 
+            // setupPage
+            // 
+            this.setupPage.ImageKey = "icons8-tools-26.png";
+            this.setupPage.Location = new System.Drawing.Point(4, 39);
+            this.setupPage.Name = "setupPage";
+            this.setupPage.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.setupPage.Size = new System.Drawing.Size(786, 340);
+            this.setupPage.TabIndex = 0;
+            this.setupPage.Text = "Setup";
+            this.setupPage.UseVisualStyleBackColor = true;
+            // 
             // tentControlPage
             // 
-            this.tentControlPage.Location = new System.Drawing.Point(4, 25);
+            this.tentControlPage.ImageKey = "icons8-camping-tent-26.png";
+            this.tentControlPage.Location = new System.Drawing.Point(4, 39);
+            this.tentControlPage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tentControlPage.Name = "tentControlPage";
-            this.tentControlPage.Size = new System.Drawing.Size(1051, 442);
+            this.tentControlPage.Size = new System.Drawing.Size(786, 340);
             this.tentControlPage.TabIndex = 2;
             this.tentControlPage.Text = "Tent Control";
             this.tentControlPage.UseVisualStyleBackColor = true;
             // 
             // lightControlPage
             // 
-            this.lightControlPage.Location = new System.Drawing.Point(4, 25);
+            this.lightControlPage.ImageKey = "icons8-idea-26.png";
+            this.lightControlPage.Location = new System.Drawing.Point(4, 39);
+            this.lightControlPage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lightControlPage.Name = "lightControlPage";
-            this.lightControlPage.Size = new System.Drawing.Size(1051, 442);
+            this.lightControlPage.Size = new System.Drawing.Size(786, 340);
             this.lightControlPage.TabIndex = 3;
             this.lightControlPage.Text = "Light Control";
             this.lightControlPage.UseVisualStyleBackColor = true;
             // 
             // energyPage
             // 
-            this.energyPage.Location = new System.Drawing.Point(4, 25);
+            this.energyPage.ImageKey = "icons8-leaf-26.png";
+            this.energyPage.Location = new System.Drawing.Point(4, 39);
+            this.energyPage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.energyPage.Name = "energyPage";
-            this.energyPage.Size = new System.Drawing.Size(1051, 442);
+            this.energyPage.Size = new System.Drawing.Size(786, 340);
             this.energyPage.TabIndex = 4;
             this.energyPage.Text = "Energy";
             this.energyPage.UseVisualStyleBackColor = true;
             // 
             // emergencyPage
             // 
-            this.emergencyPage.Location = new System.Drawing.Point(4, 25);
+            this.emergencyPage.ImageKey = "icons8-siren-26.png";
+            this.emergencyPage.Location = new System.Drawing.Point(4, 39);
+            this.emergencyPage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.emergencyPage.Name = "emergencyPage";
-            this.emergencyPage.Size = new System.Drawing.Size(1051, 442);
+            this.emergencyPage.Size = new System.Drawing.Size(786, 340);
             this.emergencyPage.TabIndex = 5;
             this.emergencyPage.Text = "Emergency";
             this.emergencyPage.UseVisualStyleBackColor = true;
             // 
             // explorePage
             // 
-            this.explorePage.Location = new System.Drawing.Point(4, 25);
+            this.explorePage.ImageKey = "icons8-compass-26.png";
+            this.explorePage.Location = new System.Drawing.Point(4, 39);
+            this.explorePage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.explorePage.Name = "explorePage";
-            this.explorePage.Size = new System.Drawing.Size(1051, 442);
+            this.explorePage.Size = new System.Drawing.Size(786, 340);
             this.explorePage.TabIndex = 6;
             this.explorePage.Text = "Explore";
             this.explorePage.UseVisualStyleBackColor = true;
             // 
             // orderPage
             // 
-            this.orderPage.Location = new System.Drawing.Point(4, 25);
+            this.orderPage.ImageKey = "icons8-restaurant-26.png";
+            this.orderPage.Location = new System.Drawing.Point(4, 39);
+            this.orderPage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.orderPage.Name = "orderPage";
-            this.orderPage.Size = new System.Drawing.Size(1051, 442);
+            this.orderPage.Size = new System.Drawing.Size(786, 340);
             this.orderPage.TabIndex = 7;
             this.orderPage.Text = "Order";
             this.orderPage.UseVisualStyleBackColor = true;
             // 
             // eventsPage
             // 
-            this.eventsPage.Location = new System.Drawing.Point(4, 25);
+            this.eventsPage.ImageKey = "icons8-schedule-26.png";
+            this.eventsPage.Location = new System.Drawing.Point(4, 74);
+            this.eventsPage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.eventsPage.Name = "eventsPage";
-            this.eventsPage.Size = new System.Drawing.Size(1051, 442);
+            this.eventsPage.Size = new System.Drawing.Size(786, 305);
             this.eventsPage.TabIndex = 8;
             this.eventsPage.Text = "Events";
             this.eventsPage.UseVisualStyleBackColor = true;
             // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "icons8-schedule-26.png");
+            this.imageList1.Images.SetKeyName(1, "icons8-restaurant-26.png");
+            this.imageList1.Images.SetKeyName(2, "icons8-compass-26.png");
+            this.imageList1.Images.SetKeyName(3, "icons8-siren-26.png");
+            this.imageList1.Images.SetKeyName(4, "icons8-leaf-26.png");
+            this.imageList1.Images.SetKeyName(5, "icons8-tools-26.png");
+            this.imageList1.Images.SetKeyName(6, "icons8-idea-26.png");
+            this.imageList1.Images.SetKeyName(7, "icons8-camping-tent-26.png");
+            this.imageList1.Images.SetKeyName(8, "icons8-home-26.png");
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(900, 550);
             this.Controls.Add(this.TabMaster);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DrawerShowIconsWhenHidden = true;
+            this.DrawerTabControl = this.TabMaster;
             this.Name = "Form1";
-            this.Padding = new System.Windows.Forms.Padding(4, 79, 4, 4);
             this.Text = "TrailMate";
             this.TabMaster.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -173,6 +206,7 @@
         private System.Windows.Forms.TabPage explorePage;
         private System.Windows.Forms.TabPage orderPage;
         private System.Windows.Forms.TabPage eventsPage;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
